1e1a4d1c993e59f198a64303c609ed05acef65d6
Ranna <user@Rannas-Mac.local>
2018-03-15 01:21:42 +0900

    mac starter sh commit

--- startup_local_server_on_mac.sh
+++ startup_local_server_on_mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk http://localhost:8000 --disable-web-security --user-data-dir=/tmp/someDir/ &
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://localhost:8000 --disable-web-security --user-data-dir=/tmp/someDir/ &
 python -m SimpleHTTPServer
