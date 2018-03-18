17ac50fd156d30c8aee5d539348dd98bf070c110
Ranna <user@Rannas-Mac.local>
2018-03-19 01:52:14 +0900

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 aws s3 cp index.html s3://laundry-monitoring-service-website-prod --acl public-read
 aws s3 cp default.jpg s3://laundry-monitoring-service-website-prod --acl public-read
-aws s3 cp css/style.css s3://laundry-monitoring-service-website-prod --acl public-read
+aws s3 cp css/style.css s3://laundry-monitoring-service-website-prod/css/ --acl public-read
 aws s3 cp index.js s3://laundry-monitoring-service-website-prod --acl public-read
 aws s3 cp icon/favicon.ico s3://laundry-monitoring-service-website-prod/icon/ --acl public-read
 aws s3 cp icon/logo.jpg s3://laundry-monitoring-service-website-prod/icon/ --acl public-read
